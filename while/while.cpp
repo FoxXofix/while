da8bdbf36baf38e8727d970d9ae5849bec88b065
Александр Важнёв <vazhnevalex@gmail.com>
2025-02-24 17:39:37 +0300

    Добавьте файлы проекта.

---
+++ while/while.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int number, id = 1;
+	cout << "задайте номер = ";
+	cin >> number;
+	while (id <= number)                 // пока id меньше или равно number
+	{
+		cout << id++ << "\t";            // выводит на экран значение id + прибовляет к нему еденицу
+	}
+}
